b5d1ad640d0548c1166eeb74fbebe7bb1f9c6d5a
danbudris <d.c.budris@gmail.com>
2018-01-27 14:00:04 -0500

    finished module, added chai tests

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 const temps = require("./src/temperatures.js");
+const should = require('chai').should()
 
-temps.Fahrenheit(50, 'c');
-temps.Celsius(50, 'f');
+/* Celcius to Fahrenheit */
+temps.Fahrenheit(50, 'c').should.equal(122.00);
+
+/* Fahrenheit to Kelvin */
+temps.Fahrenheit(50, 'k').should.equal(283.15);
+
+/* Fahrenheit to Celsius */
+temps.Celsius(50, 'f').should.equal(10.00);
+
+/* Celsius to Kelvin */
+temps.Celsius(50, 'k').should.equal(323.15);
--- src/temperatures.js
+++ src/temperatures.js
@@ -1,23 +1,27 @@
 const math = require('mathjs');
 
 convertF = (temp, mode) => {
+  /* Celsius to Fahrenheit */
   if (mode == "c") {
-    returntmp = (temp - 32) * math.fraction('5/9'); 
+    returntmp = (temp * math.fraction('9/5')) + 32;
   }
+  /* Fahrenheit to Kelvin */
   if (mode == "k") {
     returntmp = (temp + 459.67) * math.fraction('5/9');
   }
-  return returntmp;
+  return parseFloat(returntmp.toFixed(2));
 };
 
 convertC = (temp, mode) => {
+  /* Fahrenheit to Celsius */
   if (mode == "f"){
-    returntmp = (temp * math.fraction('9/5')) + 32;
+    returntmp = (temp - 32) * math.fraction('5/9'); 
   }
+  /* Celsius to Kelvin */
   if (mode == "k"){
     returntmp = temp + 273.15
   }
-  return returntmp;
+  return parseFloat(returntmp.toFixed(2));
 };
 
 module.exports = {
